8c7d53a2003645a43935c4ab7342026259844bab
Vivek-269 <vsv2691998@gmail.com>
2023-03-30 12:17:28 +0530

    first commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      Following
+    <div style={{}}>
+      <h2>Following web tech are used in the project:</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>ReactJS</li>
+      </ul>
     </div>
   )
 }
